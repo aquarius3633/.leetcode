351fe1862e0f17f0473deb17e86e2ab0739f7959
WENQIANG SHU <aquarius3633@gmail.com>
2022-06-20 18:10:23 +1000

    "add Two numbers" Solved

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -18,7 +18,38 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        ListNode* node = new ListNode();
+        ListNode* head = node;
+        int sum;
+        int carry = 0;
+        while(l1 != nullptr && l2 != nullptr) {
+            sum = l1->val + l2->val + carry;
+            carry = sum / 10;
+            node->next = new ListNode(sum%10);
+            l1 = l1->next;
+            l2 = l2->next;
+            node = node->next;
+        }
+
+        while(l1 != nullptr){
+            sum = l1->val +carry;
+            carry = sum / 10;
+            node->next = new ListNode(sum%10);
+            l1 = l1->next;
+            node = node->next;
+        }
+        while(l2 != nullptr){
+            sum = l2->val +carry;
+            carry = sum / 10;
+            node->next = new ListNode(sum%10);
+            l2 = l2->next;
+            node = node->next;
+        }
+        if(carry){
+            node->next = new ListNode(carry);
+        }
+
+        return head->next;
     }
 };
 // @lc code=end
